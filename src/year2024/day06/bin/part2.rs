d117f0933d4c71c9b675b7fbf8e55e2b2b94874a
Clement <vasseur.clement@gmail.com>
2024-12-15 18:30:34 -0500

    2024 day 6 part2

--- src/year2024/day06/bin/part2.rs
+++ src/year2024/day06/bin/part2.rs
@@ -5,6 +5,7 @@ use adv_code::year2024::day06::lib::Termination::Loop;
 use adv_code::*;
 use anyhow::*;
 use code_timing_macros::time_snippet;
+use std::collections::HashSet;
 
 const INPUT_FILE: &str = "input/2024/06/inputs.txt";
 
@@ -14,7 +15,7 @@ fn main() -> Result<()> {
     let result = time_snippet!(part2(INPUT_FILE));
     println!("Result = {}", result);
 
-    // assert_eq!(result, 1);
+    assert_eq!(result, 1836);
 
     Ok(())
 }
@@ -23,8 +24,13 @@ fn part2(file_input_path: &str) -> usize {
     let (guard_position, mut grid) = parse_input(&file_input_path);
     let (path, _) = get_path(guard_position, &grid);
 
+    let mut already_blocked = HashSet::new();
     let mut looped_path_count = 0;
     for (index, (path_coordinates, _)) in (&path).into_iter().enumerate().skip(1) {
+        if !already_blocked.insert(path_coordinates) {
+            continue;
+        }
+
         set_grid_element(&mut grid, path_coordinates, '#');
         let start_guard_position = path.get(index - 1).unwrap();
 
@@ -41,6 +47,9 @@ fn part2(file_input_path: &str) -> usize {
 mod test {
     use crate::parse_input;
     use crate::part2;
+    use adv_code::lib::grid_utils::Direction::{East, North, South};
+    use adv_code::year2024::day06::lib::parser::GuardPosition;
+    use std::collections::HashSet;
 
     const TEST_INPUT_FILE: &str = "input/2024/06/test_inputs_part2.txt";
 
@@ -53,5 +62,24 @@ mod test {
     fn test_parser() {
         let parsed = parse_input(TEST_INPUT_FILE);
         println!("Result = {:?}", parsed);
+    }
+
+    #[test]
+    fn test_set() {
+        let mut set: HashSet<GuardPosition> = HashSet::new();
+
+        set.insert(((0, 0).into(), North));
+        let insert_south = set.insert(((0, 0).into(), South));
+        let insert_south_again = set.insert(((0, 0).into(), South));
+        let insert_south_different = set.insert(((1, 0).into(), South));
+
+        assert!(insert_south);
+        assert!(insert_south_different);
+        assert!(!insert_south_again);
+        assert!(set.contains(&((0, 0).into(), South)));
+        assert!(set.contains(&((1, 0).into(), South)));
+        assert!(set.contains(&((0, 0).into(), North)));
+        assert!(!set.contains(&((0, 0).into(), East)));
+        assert!(!set.contains(&((1, 1).into(), South)));
     }
 }
--- src/year2024/day06/lib/mod.rs
+++ src/year2024/day06/lib/mod.rs
@@ -1,4 +1,4 @@
-use crate::lib::grid_utils::{Coordinates, Direction, GridCoordinates};
+use crate::lib::grid_utils::Coordinates;
 use crate::year2024::day06::lib::parser::GuardPosition;
 use grid::Grid;
 use std::collections::{HashSet, VecDeque};
@@ -16,20 +16,21 @@ pub fn get_path(
     grid: &Grid<char>,
 ) -> (VecDeque<GuardPosition>, Termination) {
     let mut path = VecDeque::new();
-    path.push_back((start_guard_coordinates, start_direction));
     let mut visited = HashSet::new();
 
     let mut guard_coordinates = start_guard_coordinates;
     let mut direction = start_direction;
     loop {
+        let guard_position = (guard_coordinates, direction);
+        if !visited.insert((guard_position, direction)) {
+            return (path, Termination::Loop);
+        }
+        path.push_back(guard_position.clone());
+
         let new_coordinates_option = guard_coordinates.move_to(direction);
         let new_coordinates = match new_coordinates_option {
-            Some(coordinates) => {
-                coordinates
-            }
+            Some(coordinates) => coordinates,
             None => {
-                let guard_position = (guard_coordinates, direction);
-                path.push_back(guard_position);
                 return (path, Termination::ExitedMap);
             }
         };
@@ -37,11 +38,6 @@ pub fn get_path(
         let map_element = grid.get(new_coordinates.row(), new_coordinates.column());
         match map_element {
             Some('.') => {
-                let guard_position = (new_coordinates, direction);
-                if !visited.insert(guard_position.clone()) {
-                    return (path, Termination::Loop);
-                }
-                path.push_back(guard_position);
                 guard_coordinates = new_coordinates;
             }
             Some('#') => {
@@ -49,7 +45,6 @@ pub fn get_path(
             }
             Some(_) => panic!("Unexpected element in map"),
             None => {
-                path.push_back((guard_coordinates, direction));
                 return (path, Termination::ExitedMap);
             }
         }
