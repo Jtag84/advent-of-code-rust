5efd262feb914a113d17d576fb61847838a72354
Clement <vasseur.clement@gmail.com>
2025-01-06 10:34:15 -0500

    2024 day 20 part 2

--- src/year2024/day20/bin/part2.rs
+++ src/year2024/day20/bin/part2.rs
@@ -1,26 +1,65 @@
+use adv_code::lib::grid_utils::Coordinates;
 use adv_code::year2024::day20::lib::parser::parse_input;
 use adv_code::*;
 use anyhow::*;
 use code_timing_macros::time_snippet;
-use itertools::Itertools;
-use std::iter::zip;
+use pathfinding::prelude::astar;
 
 const INPUT_FILE: &str = "input/2024/20/inputs.txt";
 
 fn main() -> Result<()> {
     start_day(2024, 20, 2);
 
-    let result = time_snippet!(part2(INPUT_FILE));
+    let result = time_snippet!(part2(INPUT_FILE, 100));
     println!("Result = {}", result);
 
-    assert_eq!(result, 1);
+    assert_eq!(result, 982891);
 
     Ok(())
 }
 
-fn part2(file_input_path: &str) -> isize {
-    let parsed = parse_input(&file_input_path);
-    0
+fn part2(file_input_path: &str, cheat_cost_saving: usize) -> usize {
+    calculate_count_of_cheats_saving(&file_input_path, cheat_cost_saving, 20)
+}
+
+// This method could also be used for part1, but the current part1 implementation is much faster
+fn calculate_count_of_cheats_saving(
+    file_input_path: &&str,
+    cheat_cost_saving: usize,
+    cheat_max_length: usize,
+) -> usize {
+    let (racetrack, start, end) = parse_input(&file_input_path);
+    let (no_cheat_path, _) = astar(
+        &start,
+        |node| {
+            node.cardinals().into_iter().filter_map(|c| {
+                racetrack
+                    .get(c.row(), c.column())
+                    .filter(|v| **v == '.')
+                    .map(|_| (c, 1))
+            })
+        },
+        |node| node.manhattan_distance(end),
+        |node| *node == end,
+    )
+    .unwrap();
+
+    let mut connecting_path_with_cheat_count = 0;
+    for (from_index, from_coordinates) in no_cheat_path.clone().into_iter().enumerate() {
+        for (to_index, to_coordinates) in no_cheat_path
+            .clone()
+            .into_iter()
+            .enumerate()
+            .skip(from_index + cheat_cost_saving)
+        {
+            let manhattan_distance = from_coordinates.manhattan_distance(to_coordinates);
+            let cost_saving = to_index - from_index - manhattan_distance;
+            if manhattan_distance <= cheat_max_length && cost_saving >= cheat_cost_saving {
+                connecting_path_with_cheat_count += 1
+            }
+        }
+    }
+    connecting_path_with_cheat_count
 }
 
 #[cfg(test)]
@@ -32,7 +71,7 @@ mod test {
 
     #[test]
     fn part2_test() {
-        assert_eq!(part2(TEST_INPUT_FILE), 1);
+        assert_eq!(part2(TEST_INPUT_FILE, 70), 41);
     }
 
     #[test]
